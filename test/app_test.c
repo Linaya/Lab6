68cab0d2fd415cd7353e0fe85bf988d2a2416bb3
Linaya <Angelochec6@gmail.com>
2016-06-10 20:44:42 +0600

    noviy test

---
+++ test/app_test.c
@@ -0,0 +1,12 @@
+#include <ctest.h>
+#include <app.h>
+
+CTEST(app_suite, nuli)
+{
+    const double a = 0, b = 0, c = 0;
+    double d, x1, x2;
+
+    const double koren = uravn(a, b, c, &d, &x1, &x2), ex_koren = -1;
+
+    ASSERT_EQUAL(ex_koren, koren);
+}
