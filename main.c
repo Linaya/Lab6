551ed7f455470c7e113309e5a1512346bbb683b8
Linaya <Angelochec6@gmail.com>
2016-06-10 20:17:18 +0600

    reshenie

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-	double a, b, c, d, x1, x2;
-	int koren;
-	printf("vvedite a b c: ");
-	scanf("%lf %lf %lf", a, b, c);
+    double a, b, c, d, x1, x2;
+    int koren;
+    printf("vvedite a b c: ");
+    scanf("%lf %lf %lf", a, b, c);
+    
+    if (a == 0)
+        return -1;
+    d = (b * b) - (4 * a * c);
+    if (d < 0)
+        return 0;
+    else if (d == 0)
+    {
+        x1 = -b / (2 * a);
+        return 1;
+    }
+    x1 = (-b + sqrt(d)) / (2 * a);
+    x2 = (-b - sqrt(d)) / (2 * a);
+    return 2;
 }
