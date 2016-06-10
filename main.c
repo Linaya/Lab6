d1fc9e030e35afe0ce9cea63c6b81817906b267a
Linaya <Angelochec6@gmail.com>
2016-06-10 20:31:33 +0600

    faily

--- main.c
+++ main.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
-#include <math.h>
+#include "app.h"
 
 int main()
 {
-    double a, b, c, d, x1, x2;
-    printf("vvedite a b c: ");
-    scanf("%lf %lf %lf", &a, &b, &c);
-    
-    if (a == 0)
-    {
+	double a, b, c, d, x1, x2;
+	int koren;
+	printf("vvedite a b c: ");
+	scanf("%lf %lf %lf", &a, &b, &c);
+	koren = uravn(a, b, c, &d, &x1, &x2);
+	if (koren == -1)
+	{
         printf("A dolzhen byt ne raven nulyu.\n");
-        return -1;
-    }
-    d = (b * b) - (4 * a * c);
-    if (d < 0)
+		return 0;
+	}
+
+	printf("D: %lf\n", d);
+
+	if (koren == 0)
+		printf("Korney net.\n");
+	else if (koren == 1)
+		printf("X: %lf\n", x1);
+    else
     {
-        printf("D: %lf\n", d);
-        printf("Korney net.\n");
-        return 0;
-    }
-    else if (d == 0)
-    {
-        x1 = -b / (2 * a);
-        printf("D: %lf\n", d);
         printf("X1: %lf\n", x1);
-        return 1;
+        printf("X2: %lf\n", x2);
     }
-    x1 = (-b + sqrt(d)) / (2 * a);
-    x2 = (-b - sqrt(d)) / (2 * a);
-    printf("D: %lf\n", d);
-    printf("X1: %lf\n", x1);
-    printf("X2: %lf\n", x2);
-    return 2;
+    
+    return 0;
 }
