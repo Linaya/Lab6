f261660e44777d9624b2e78aa5cc5fa4272581e5
Linaya <Angelochec6@gmail.com>
2016-06-10 20:18:49 +0600

    vyvod

--- main.c
+++ main.c
@@ -9,16 +9,28 @@ int main()
     scanf("%lf %lf %lf", a, b, c);
     
     if (a == 0)
+    {
+        printf("A dolzhen byt ne raven nulyu.\n");
         return -1;
+    }
     d = (b * b) - (4 * a * c);
     if (d < 0)
+    {
+        printf("D: %lf\n", d);
+        printf("Korney net.\n");
         return 0;
+    }
     else if (d == 0)
     {
         x1 = -b / (2 * a);
+        printf("D: %lf\n", d);
+        printf("X1: %lf\n", x1);
         return 1;
     }
     x1 = (-b + sqrt(d)) / (2 * a);
     x2 = (-b - sqrt(d)) / (2 * a);
+    printf("D: %lf\n", d);
+    printf("X1: %lf\n", x1);
+    printf("X2: %lf\n", x2);
     return 2;
 }
